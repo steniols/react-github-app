1c65c0bc710c983dfa63363643ecb8e0b5adb3f8
Stenio L. de Souza <steniols@gmail.com>
2021-04-29 10:32:39 -0300

    deploy configs

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./src/config/express");
 const hostname = "localhost";
 const port = process.env.PORT || 8003;
 
-app.listen(PORT, () => {
-  console.log(`server has started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server has started on port ${port}`);
 });
