1cb9546a33751ec2bf73b49812116c83901c313d
Stenio L. de Souza <steniols@gmail.com>
2021-05-05 18:49:46 -0300

    deploy configs

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   production: {
     client: "pg",
-    connection: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
+    connection: `postgres://ligashmowiwuuc:d15962d95a61676f0876f9e503dbe8fc631d5137cb928fcc3f8b5413e463ffbb@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d68ghstahvs7qs`,
     migrations: {
       directory: "migrations",
       tableName: "knex_migrations",
