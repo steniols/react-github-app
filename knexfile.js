c8683eb7cf17aae991ca8bb075d3bbce43ab9ccc
Stenio L. de Souza <steniols@gmail.com>
2021-05-05 19:06:48 -0300

    deploy configs

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,8 @@ module.exports = {
   production: {
     client: "pg",
     connection:
-      process.env.DATABASE_URL ||
-      `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
-    migrations: {
-      directory: "migrations",
-      tableName: "knex_migrations",
-    },
-    ssl: true,
+      `${process.env.DATABASE_URL}?ssl=true` ||
+      `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}?ssl=true`,
   },
   development: {
     client: "pg",
