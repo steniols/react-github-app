d1eacd35e0d5417de763dfe99549fea6f842d73a
Stenio L. de Souza <steniols@gmail.com>
2021-05-12 12:30:53 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,8 +9,8 @@ Cypress.Commands.add("githubLogin", () => {
 
   cy.get("body").then(($body) => {
     if ($body.find("#login_field").length > 0) {
-      cy.get("#login_field").type("steniols");
-      cy.get("#password").type("stpnk9524");
+      cy.get("#login_field").type("");
+      cy.get("#password").type("");
       cy.get(".btn").click();
     }
   });
