1c5007b738f988e02fc39b08c7bd071f098f3493
Stenio L. de Souza <steniols@gmail.com>
2021-12-09 17:50:32 -0300

    checking variable on page load

--- client/src/services/github.service.js
+++ client/src/services/github.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import env from "react-dotenv";
 
-const apiUrl = env.API_URL ? `${env.API_URL}/github` : "/github";
+const apiUrl = env && env.API_URL ? `${env.API_URL}/github` : "/github";
 
 const githubService = {
   async getUser() {
